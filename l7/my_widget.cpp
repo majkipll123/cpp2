dad7112820b9edf0ef008c5e658c64dc145cfb3e
majkipll123 <115625416+majkipll123@users.noreply.github.com>
2023-06-12 13:32:04 +0200

    Add files via upload

--- l7/my_widget.cpp
+++ l7/my_widget.cpp
@@ -1,26 +1,83 @@
-QT       += core gui
 
-greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+#include "my_widget.h"
 
-CONFIG += c++11
+#include <QPainter>
 
-# You can make your code fail to compile if it uses deprecated APIs.
-# In order to do so, uncomment the following line.
-#DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
+#include <QMouseEvent>
 
-SOURCES += \
-    main.cpp \
-    mainwindow.cpp \
-    my_widget.cpp
+MyWidget::MyWidget(QWidget *parent) : QWidget(parent) {
 
-HEADERS += \
-    mainwindow.h \
-    my_widget.h
+}
 
-FORMS += \
-    mainwindow.ui
+void MyWidget::paintEvent(QPaintEvent *event) {
 
-# Default rules for deployment.
-qnx: target.path = /tmp/$${TARGET}/bin
-else: unix:!android: target.path = /opt/$${TARGET}/bin
-!isEmpty(target.path): INSTALLS += target
+    QPainter painter(this);
+
+    painter.setRenderHint(QPainter::Antialiasing);
+
+    painter.setBrush(QBrush(QColor(Qt::GlobalColor::darkMagenta)));
+
+    auto r = this->rect();
+
+    painter.drawRect(r);
+
+    for (const auto &circ : circles) {
+
+        painter.setBrush(QBrush(circ.color)); // Ustawienie koloru koła
+
+        QPointF point(circ.center.x() * r.width(), circ.center.y() * r.height());
+
+        painter.drawEllipse(point, circ.radius, circ.radius);
+
+    }
+
+    QWidget::paintEvent(event);
+
+}
+
+void MyWidget::mousePressEvent(QMouseEvent *event) {
+
+    auto pos = event->localPos();
+
+    auto rec = rect();
+
+    Circle c;
+
+    c.radius = DEFAULT_RADIUS;
+
+    c.center.setX(pos.x() / rec.width());
+
+    c.center.setY(pos.y() / rec.height());
+
+    c.color = QColor(Qt::GlobalColor::darkYellow); // Ustawienie domyślnego koloru koła
+
+    circles.push_back(c);
+
+    update();
+
+}
+
+void MyWidget::KeyPressed() {
+
+    if (!circles.empty()) {
+
+        circles.pop_back();
+
+    }
+
+}
+
+void MyWidget::wheelEvent(QWheelEvent *event) {
+
+    for (auto &circ : circles) {
+
+        circ.color = QColor(rand()%256, rand()%256, rand()%256); // Ustawienie losowego koloru koła
+
+    }
+
+    update();
+
+    event->accept();
+
+}
+
